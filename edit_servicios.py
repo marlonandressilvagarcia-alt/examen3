d2974e523fef3ef72ce26f6803ee1f6cea147834
Julian Rolon <yuliniri200712@gmail.com>
2026-05-04 20:30:29 -0500

    correccion de codigo

---
+++ edit_servicios.py
@@ -0,0 +1,6 @@
+class Servicio:
+    def __init__(self, nombre, precio, tipo_evento, duracion):
+        self.nombre = nombre
+        self.precio = precio
+        self.tipo_evento = tipo_evento
+        self.duracion = duracion
